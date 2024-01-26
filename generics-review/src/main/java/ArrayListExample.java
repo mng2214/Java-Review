17f6ac42c6629f75e8a38bfc2968c2635797b1db
mng2214 <your_email@example.com>
2024-01-25 20:57:13 -0600

    generics review added

--- generics-review/src/main/java/ArrayListExample.java
+++ generics-review/src/main/java/ArrayListExample.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ArrayListExample {
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> items = new ArrayList<>();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        // items.add("4");
+
+        printDouble(items);
+
+    }
+
+    private static void printDouble(ArrayList<Integer> items) {
+
+//        for (Object number : items) {
+//            System.out.println((Integer) number * 2 );
+//        }
+
+        for (int number : items) {
+            System.out.println(number * 2);
+        }
+    }
 }
