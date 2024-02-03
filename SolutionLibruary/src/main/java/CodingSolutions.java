edc0546d327f19aa477df5b2282de0cb76591331
mng2214 <your_email@example.com>
2024-02-03 15:32:44 -0600

    Solution added

--- SolutionLibruary/src/main/java/CodingSolutions.java
+++ SolutionLibruary/src/main/java/CodingSolutions.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CodingSolutions {
 
@@ -35,5 +32,40 @@ public class CodingSolutions {
         return null;
     }
 
+    // Given an array of int and target num. Return indexes of a number from array that will give result as target
+    // Input [2,7,11,15] target = 9
+    // Output [0,1]
+
+    public static int[] indexOfSumFinder(int[] arr, int target) {
+        // int [] result = new int[2];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    // OR
+
+    public static int[] indexOfSumFinderOptimal(int[] arr, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int potentialMatch = target-arr[i];
+            if(map.containsKey(potentialMatch)) return new int[]{i,potentialMatch};
+            else map.put(arr[i],i);
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString( indexOfSumFinder(arr, target)));
+    }
 
 }
